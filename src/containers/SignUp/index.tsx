4a8bb5d6b2a67859a4664d86345bdab5c49373ad
ahnytae <ahnheetae@ucomp.co.kr>
2021-03-14 14:15:59 +0900

    :alien: change server port
    서버포트 변경

--- src/containers/SignUp/index.tsx
+++ src/containers/SignUp/index.tsx
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
-import {gql, useMutation} from '@apollo/client';
+import React, { useState } from 'react';
+import { gql, useMutation } from '@apollo/client';
 
 const SIGNUP = gql`
   mutation createUser($id: String!, $pw: String!, $name: String!) {
-    createUser(variables: {id: $id, pw: $pw, name: $name}) {
+    createUser(variables: { id: $id, pw: $pw, name: $name }) {
       userNo
       id
       pw
@@ -19,25 +19,25 @@ const SignUp: React.FC = (): any => {
     name: '',
   });
 
-  const [signup, {loading, data}] = useMutation(SIGNUP);
+  const [signup, { loading, data }] = useMutation(SIGNUP);
 
   if (loading) return 'Loading...';
 
   const onChange = (e: any) => {
-    const {name, value} = e.target;
-    setAccountInfo({...accountInfo, [name]: value});
+    const { name, value } = e.target;
+    setAccountInfo({ ...accountInfo, [name]: value });
   };
 
   const authCheck = (e: any) => {
-    const {id, pw, name} = accountInfo;
+    const { id, pw, name } = accountInfo;
     e.preventDefault();
-    signup({variables: {id, pw, name}});
+    signup({ variables: { id, pw, name } });
 
     if (data) alert('success!');
     else alert('fail!');
   };
 
-  const {id, pw, name} = accountInfo;
+  const { id, pw, name } = accountInfo;
   return (
     <div>
       <h2>회원가입</h2>
@@ -45,7 +45,7 @@ const SignUp: React.FC = (): any => {
         <input name="id" value={id} type="text" placeholder="ID" onChange={onChange} />
         <input name="pw" value={pw} type="password" placeholder="PW" onChange={onChange} />
         <input name="name" value={name} type="text" placeholder="name" onChange={onChange} />
-        <button onClick={e => authCheck(e)}>회원가입</button>
+        <button onClick={(e) => authCheck(e)}>회원가입</button>
       </form>
     </div>
   );
