210fc95aeb799fa45daaa3f2dff41f14d871e66d
전병길 <58627894+jeonbg@users.noreply.github.com>
2021-03-14 15:01:47 +0900

    Revert "LoginPage StyleTest"

--- src/containers/SignIn/index.tsx
+++ src/containers/SignIn/index.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { gql, useLazyQuery } from '@apollo/client';
-import styled from 'styled-components';
 
 export const LOGIN = gql`
   query($id: String!) {
@@ -25,50 +24,16 @@ const SignIn: React.FC = (): any => {
   };
 
   return (
-    <LoginPage>
-      <div className="login-wrapper">
-        <h2>Login</h2>
-        <LoginForm onSubmit={authCheck}>
-          <input type="text" placeholder="ID" onChange={(e) => setId(e.target.value)} />
-          <input type="password" placeholder="PW" />
-          <button type="submit">Login</button>
-        </LoginForm>
-        {data && <h1>SUCC</h1>}
-      </div>
-    </LoginPage>
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={authCheck}>
+        <input type="text" placeholder="ID" onChange={(e) => setId(e.target.value)} />
+        <input type="password" placeholder="PW" />
+        <button type="submit">Login</button>
+      </form>
+      {data && <h1>SUCC</h1>}
+    </div>
   );
 };
 
 export default SignIn;
-
-const LoginPage = styled.div`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  position:fixed;
-  top:0;
-  left:0;
-  width: 100vw;
-  height:100vh;
-  .login-wrapper {
-    width:768px;
-    > h2 {
-      text-align:center;
-    }
-  }
-`;
-
-const LoginForm = styled.form`
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  flex-direction:column;
-  > input {
-    display:block;
-    width: 300px;
-  }
-  > button {
-    display:inline-block;
-    width:300px;
-  }
-`;
